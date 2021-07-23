0a9792e1ceb24876cb751b5b9a6eb2dd3322421e
Aleksey <lexdesigner.as@gmail.com>
2021-07-23 16:30:34 +0300

    Add files via upload

---
+++ exeedrostokino/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
+    theme: {
+        extend: {
+            fontSize: {
+                'xxs': '0.5rem'
+            },
+            colors: {
+                'exeed-dark': '#1B1D23',
+                'dark-gray': '#30363A',
+                'light-gray': '#C9CACA',
+                'bronze': '#C78E66',
+                'exeed-orange': '#ED6D1F',
+                'turquoise': '#26B7BC',
+                'dark-blue': '#242834'
+            },
+        }
+    },
+    variants: {},
+    plugins: [],
+  }
